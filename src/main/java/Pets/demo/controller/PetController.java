fccbe96a80ce0bdb3b6ada938b244b4e68b1b341
PIkOOESP <miguelangel.s.b86@gmail.com>
2025-09-26 00:01:59 +0200

    Frontend cutre

--- src/main/java/Pets/demo/controller/PetController.java
+++ src/main/java/Pets/demo/controller/PetController.java
@@ -3,12 +3,14 @@ package Pets.demo.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import Pets.demo.entity.Pets;
 import Pets.demo.repository.PetRepository;
 
 @RestController
+@RequestMapping("/pets")
 public class PetController {
 
     private PetRepository petRepository;
@@ -17,7 +19,7 @@ public class PetController {
         this.petRepository = petRepository;
     }
     
-    @GetMapping
+    @GetMapping("/list")
     public List<Pets> hello() {
         return petRepository.findAll();
     }
