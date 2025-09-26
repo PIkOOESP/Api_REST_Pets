d0657e9f820b73da61f1def8b89e607d2227d9a3
PIkOOESP <miguelangel.s.b86@gmail.com>
2025-09-26 16:30:20 +0200

    Base de datos en linea

--- src/main/java/Pets/demo/controller/PetController.java
+++ src/main/java/Pets/demo/controller/PetController.java
@@ -8,17 +8,31 @@ import org.springframework.web.bind.annotation.RestController;
 
 import Pets.demo.entity.Pets;
 import Pets.demo.repository.PetRepository;
-
+/**
+ * <p>En este controlador se exponen todos los endpoints relacionados con pets. {@link PetRepository}</p>
+ * @version 1.0
+ * @author Miguel Angel Santiago
+ */
 @RestController
 @RequestMapping("/pets")
 public class PetController {
 
     private PetRepository petRepository;
 
+    /**
+     * <p>Constructor del controlador de mascotas.</p>
+     * @param petRepository repositorio para consultar la base de datos.
+     */
+
     public PetController(PetRepository petRepository) {
         this.petRepository = petRepository;
     }
     
+    /**
+     * <p>Este metodo devuelve una lista con todas las mascotas en la base de datos.</p>
+     * @return List<Pets> informacion de todas las mascotas en la base de datos.
+     */
+
     @GetMapping("/list")
     public List<Pets> hello() {
         return petRepository.findAll();
