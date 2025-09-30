44c55400ce958751f030ee2a1856f0c3d0fa6135
PIkOOESP <miguelangel.s.b86@gmail.com>
2025-09-30 19:09:08 +0200

    Al dia

---
+++ front-end/adoptar.php
@@ -0,0 +1,40 @@
+<?php
+
+$url = 'http://localhost:8080/pets/list';
+$data = file_get_contents($url);
+
+$pets = json_decode($data, true);
+$contador=0;   
+
+?>
+
+<html>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <table class="table table-striped">
+            <tr>
+                <td>ID</td>
+                <td>Nombre</td>
+                <td>Chip</td>
+                <td>Especie</td>
+                <td>Año de nacimiento</td>
+                <td>Adoptar</td>
+            </tr>
+
+            <?php
+            while (count($pets) > $contador) {
+                
+                if($pets[$contador]['adopt'] == true ) {
+                
+                } else {
+                     echo '<tr><td>'.$pets[$contador]['id'].'</td><td>'.$pets[$contador]['name'].'</td><td>'.$pets[$contador]['chip'].'</td><td>'.$pets[$contador]['category'].'</td><td>'.$pets[$contador]['born'].'</td><td><a href="accion-adoptar.php"><img width="50px" src="https://docs.usebruno.com/bruno.png"></a></td></tr>';
+                }
+                $contador++;
+            }
+            ?>
+        </table>
+    </body>
+</html>
--- front-end/index.php
+++ front-end/index.php
@@ -1,17 +1,12 @@
-<?php
-
-$url = 'http://localhost:8080/pets/list';
-$data = file_get_contents($url);
-
-$pets = json_decode($data, true);
-$contador = 0;
-echo'<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>';
-echo '<table class="table table-striped"><tr><td>Nombre</td><td>Chip</td><td>Especie</td><td>Año de nacimiento</td></tr>';
-while (count($pets) > $contador) {
-    echo '<tr><td>'.$pets[$contador]['name'].'</td><td>'.$pets[$contador]['chip'].'</td><td>'.$pets[$contador]['category'].'</td><td>'.$pets[$contador]['born'].'</td></tr>';
-    $contador++;
-}   
-
-echo '</table>';    
-?>
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Mascoteria</title>
+    </head>
+    <body>
+        <a href="adoptar.php">Lista para adoptar</a>
+        <a href="adoptados.php">Lista de adoptados</a>
+    </body>
+</html>
